e1b48140c2cdc6e475160bf24f1417922e1d0563
IGNACIO GUTMAN <46917360@ort>
2022-07-06 12:01:27 -0300

    Add Jugador.cs

---
+++ Models/Jugador.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Dinamic;
+using Microsoft.VisualBasic.CompilerServices;
+using System.Collections.Generic;
+
+namespace Jugador.Models{
+
+    public class Jugador{
+
+        private int _idJugador;
+        private int _idEquipo;
+        private string  _nombre;
+        private DateTime _fechaNacimiento;
+        private string _foto;
+        private string _equipoActual;
+
+
+        public Jugador(int pidJugador, int pidEquipo, string pnombre, DateTime pfechaNacimiento, string pfoto, string pequipoActual){
+            _idJugador = pidJugador;
+            _idEquipo = pidEquipo;
+            _nombre = pnombre;
+            _fechaNacimiento = pfechaNacimiento;
+            _foto = pfoto;
+            _equipoActual = pequipoActual;
+        }
+
+        public Jugador(){
+            _idJugador = 0;
+            _idEquipo = 0;
+            _nombre = "";
+            _fechaNacimiento = new DateTime(1, 1, 1);
+            _foto = "";
+            _equipoActual = "";
+        }
+
+        public int IdJugador{
+            get {return _idJugador;}
+            set{_idJugador = value;}
+        }
+
+        public int IdEquipo{
+            get {return _idEquipo;}
+            set{_idEquipo = value;}
+        }
+
+        public string Nombre{
+            get {return _nombre;}
+            set{_nombre = value;}
+        }
+
+        public DateTime FechaNacimiento{
+            get {return _fechaNacimiento;}
+            set{_fechaNacimiento = value;}
+        }
+
+        public string Foto{
+            get {return _foto;}
+            set{_foto = value;}
+        }
+
+        public string EquipoActual{
+            get {return _equipoActual;}
+            set{_equipoActual = value;}
+        }
+    }
+}
